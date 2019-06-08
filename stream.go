07d243215fd06d22db87c4f80599de3f6f9d6e4c
beito123 <mattcha@gmx.com>
2019-06-08 12:19:43 +0900

    Fix many comments and add missing func of streams

--- stream.go
+++ stream.go
@@ -20,7 +20,7 @@ func NewStream() *Stream {
 	return NewStreamBytes([]byte{})
 }
 
-// NewStreamBytes returns new Stream from bytes
+// NewStreamBytes returns new Stream with bytes
 func NewStreamBytes(b []byte) *Stream {
 	return &Stream{
 		buf:     b,
@@ -47,7 +47,7 @@ func (bs *Stream) Off() int {
 	return bs.off
 }
 
-// Get returns n bytes from Buffer with []byte
+// Get gets n bytes from the buffer
 func (bs *Stream) Get(n int) []byte {
 	off := bs.off
 	if n > bs.Len() {
@@ -66,7 +66,7 @@ func (bs *Stream) Put(value []byte) error {
 	return err
 }
 
-// Bytes returns the bytes left from Buffer.
+// Bytes returns the bytes left from the buffer
 func (bs *Stream) Bytes() []byte {
 	return bs.buf[bs.off:]
 }
@@ -97,7 +97,7 @@ func (bs *Stream) Skip(n int) {
 	bs.off += n
 }
 
-//Pad puts empty bytes (0x00) of le (len).
+// Pad puts empty bytes (0x00) of le (len).
 func (bs *Stream) Pad(le int) error {
 	return bs.Put(make([]byte, le))
 }
@@ -132,157 +132,187 @@ func (bs *Stream) Write(p []byte) (n int, err error) {
  *  LDouble 8bytes  Little     IEEE-754 64bits floating-point numbers
  */
 
-// Byte sets byte(unsign) got from buffer to value
+// Byte gets an unsigned byte
 func (bs *Stream) Byte() (byte, error) {
 	return ReadEByte(bs.Get(ByteSize))
 }
 
-// SByte sets byte(sign) got from buffer to value
+// SByte gets a signed byte
 func (bs *Stream) SByte() (int8, error) {
 	return ReadESByte(bs.Get(ByteSize))
 }
 
-// PutByte puts byte(unsign) from value to buffer
+// PutByte puts an unsigned byte
 func (bs *Stream) PutByte(value byte) error {
 	return bs.Put(WriteByte(value))
 }
 
-// PutSByte puts byte(sign) from value to buffer
+// PutSByte puts a signed byte
 func (bs *Stream) PutSByte(value int8) error {
 	return bs.Put(WriteSByte(value))
 }
 
-// Short sets short(unsign) got from buffer to value
+// Short gets an unsigned short
 func (bs *Stream) Short() (uint16, error) {
 	return ReadEUShort(bs.Get(ShortSize))
 }
 
-// SShort sets short(sign) got from buffer to value
+// SShort gets a signed short
 func (bs *Stream) SShort() (int16, error) {
 	return ReadEShort(bs.Get(ShortSize))
 }
 
-// LShort sets short(unsign) got from buffer as LittleEndian to value
+// LShort gets an unsigned short with LittleEndian
 func (bs *Stream) LShort() (uint16, error) {
 	return ReadELUShort(bs.Get(ShortSize))
 }
 
-// LSShort sets short(sign) got from buffer as LittleEndian to value
+// LSShort gets a signed short with LittleEndian
 func (bs *Stream) LSShort() (int16, error) {
 	return ReadELShort(bs.Get(ShortSize))
 }
 
-// PutShort puts short(unsign) from value to buffer
+// PutShort puts an unsigned short
 func (bs *Stream) PutShort(value uint16) error {
 	return bs.Put(WriteUShort(value))
 }
 
-// PutSShort puts short(sign) from value to buffer
+// PutSShort puts a signed short
 func (bs *Stream) PutSShort(value int16) error {
 	return bs.Put(WriteShort(value))
 }
 
-// PutLShort puts short(unsign) from value to buffer as LittleEndian
+// PutLShort puts an unsigned short with LittleEndian
 func (bs *Stream) PutLShort(value uint16) error {
 	return bs.Put(WriteLUShort(value))
 }
 
-// PutLSShort puts short(sign) from value to buffer as LittleEndian
+// PutLSShort puts a signed short with LittleEndian
 func (bs *Stream) PutLSShort(value int16) error {
 	return bs.Put(WriteLShort(value))
 }
 
-// Int sets int got from buffer to value
+// Int gets a signed int
 func (bs *Stream) Int() (int32, error) {
 	return ReadEInt(bs.Get(IntSize))
 }
 
-// PutInt puts int from value to buffer
+// PutInt puts a signed int
 func (bs *Stream) PutInt(value int32) error {
 	return bs.Put(WriteInt(value))
 }
 
-// UInt sets int got from buffer to value as LittleEndian
+// UInt gets an unsigned int
 func (bs *Stream) UInt() (uint32, error) {
 	return ReadEUInt(bs.Get(IntSize))
 }
 
-// PutUInt puts int from value to buffer as LittleEndian
+// PutUInt puts an unsigned int
 func (bs *Stream) PutUInt(value uint32) error {
 	return bs.Put(WriteUInt(value))
 }
 
-// LInt sets int got from buffer to value as LittleEndian
+// LInt gets a signed int with LittleEndian
 func (bs *Stream) LInt() (int32, error) {
 	return ReadELInt(bs.Get(IntSize))
 }
 
-// PutLInt puts int from value to buffer as LittleEndian
+// PutLInt puts a signed int with LittleEndian
 func (bs *Stream) PutLInt(value int32) error {
 	return bs.Put(WriteLInt(value))
 }
 
-// Long sets long got from buffer to value
+// LUInt gets an unsigned int with LittleEndian
+func (bs *Stream) LUInt() (uint32, error) {
+	return ReadELUInt(bs.Get(IntSize))
+}
+
+// PutLUInt puts an unsigned int with LittleEndian
+func (bs *Stream) PutLUInt(value uint32) error {
+	return bs.Put(WriteLUInt(value))
+}
+
+// Long gets a signed long
 func (bs *Stream) Long() (int64, error) {
 	return ReadELong(bs.Get(LongSize))
 }
 
-// PutLong puts long from value to buffer
+// PutLong puts a signed long
 func (bs *Stream) PutLong(value int64) error {
 	return bs.Put(WriteLong(value))
 }
 
-// LLong sets long got from buffer to value as LittleEndian
+// LLong gets a signed long with LittleEndian
 func (bs *Stream) LLong() (int64, error) {
 	return ReadELLong(bs.Get(LongSize))
 }
 
-// PutLLong puts long from value to buffer as LittleEndian
+// PutLLong puts a signed long with LittleEndian
 func (bs *Stream) PutLLong(value int64) error {
 	return bs.Put(WriteLLong(value))
 }
 
-// Float sets float got from buffer to value
+// ULong gets an unsigned long
+func (bs *Stream) ULong() (uint64, error) {
+	return ReadEULong(bs.Get(LongSize))
+}
+
+// PutULong puts an unsigned long
+func (bs *Stream) PutULong(value uint64) error {
+	return bs.Put(WriteULong(value))
+}
+
+// LULong gets an unsigned long with LittleEndian
+func (bs *Stream) LULong() (uint64, error) {
+	return ReadELULong(bs.Get(LongSize))
+}
+
+// PutLULong puts an unsigned long with LittleEndian
+func (bs *Stream) PutLULong(value uint64) error {
+	return bs.Put(WriteLULong(value))
+}
+
+// Float gets a float
 func (bs *Stream) Float() (float32, error) {
 	return ReadEFloat(bs.Get(FloatSize))
 }
 
-// PutFloat puts float from value to buffer
+// PutFloat puts a float
 func (bs *Stream) PutFloat(value float32) error {
 	return bs.Put(WriteFloat(value))
 }
 
-// LFloat sets float got from buffer to value as LittleEndian
+// LFloat gtes a float with LittleEndian
 func (bs *Stream) LFloat() (float32, error) {
 	return ReadELFloat(bs.Get(FloatSize))
 }
 
-// PutLFloat puts float from value to buffer as LittleEndian
+// PutLFloat puts a float
 func (bs *Stream) PutLFloat(value float32) error {
 	return bs.Put(WriteLFloat(value))
 }
 
-// Double sets double got from buffer to value
+// Double gets a double
 func (bs *Stream) Double() (float64, error) {
 	return ReadEDouble(bs.Get(DoubleSize))
 }
 
-// PutDouble puts double from value to buffer
+// PutDouble puts a double
 func (bs *Stream) PutDouble(value float64) error {
 	return bs.Put(WriteDouble(value))
 }
 
-// LDouble sets double got from buffer to value as LittleEndian
+// LDouble gets a double with LittleEndian
 func (bs *Stream) LDouble() (float64, error) {
 	return ReadELDouble(bs.Get(DoubleSize))
 }
 
-// PutLDouble puts double from value to buffer as LittleEndian
+// PutLDouble puts a double with LittleEndian
 func (bs *Stream) PutLDouble(value float64) error {
 	return bs.Put(WriteLDouble(value))
 }
 
-// Bool sets byte got from buffer as bool to value
+// Bool gets a byte and returns as bool
 func (bs *Stream) Bool() (bool, error) {
 	val, err := bs.Byte()
 	if err != nil {
@@ -292,7 +322,7 @@ func (bs *Stream) Bool() (bool, error) {
 	return val != 0, nil
 }
 
-//PutBool puts bool as byte from value to buffer
+// PutBool puts a byte as bool
 func (bs *Stream) PutBool(value bool) error {
 	var val byte
 	if value {
@@ -307,7 +337,7 @@ func NewOrderStream(order Order) *OrderStream {
 	return NewOrderStreamBytes(order, []byte{})
 }
 
-// NewOrderStreamBytes returns new Stream from bytes
+// NewOrderStreamBytes returns new Stream with bytes
 func NewOrderStreamBytes(order Order, b []byte) *OrderStream {
 	return &OrderStream{
 		Stream: NewStreamBytes(b),
@@ -315,68 +345,137 @@ func NewOrderStreamBytes(order Order, b []byte) *OrderStream {
 	}
 }
 
-// OrderStream is a binary stream with order
+// OrderStream is a binary stream with a order
 type OrderStream struct {
 	*Stream
 	Order Order
 }
 
-// Short sets short(unsign) got from buffer to value
+func (bs *OrderStream) get(size int) ([]byte, error) {
+	b := bs.Get(size)
+	if len(b) != size {
+		return nil, ErrNotEnought
+	}
+
+	return b, nil
+}
+
+// Short get an unsigned short with the order
 func (bs *OrderStream) Short() (value uint16, err error) {
-	return value, Read(bs, bs.Order, &value)
+	b, err := bs.get(ShortSize)
+	if err != nil {
+		return 0, err
+	}
+
+	return bs.Order.UShort(b), nil
 }
 
-// SShort sets short(sign) got from buffer to value
+// SShort get a signed short with the order
 func (bs *OrderStream) SShort() (value int16, err error) {
-	return value, Read(bs, bs.Order, &value)
+	b, err := bs.get(ShortSize)
+	if err != nil {
+		return 0, err
+	}
+
+	return bs.Order.Short(b), nil
 }
 
-// PutShort puts short(unsign) from value to buffer
+// PutShort puts an unsigned short with the order
 func (bs *OrderStream) PutShort(value uint16) error {
-	return Write(bs, bs.Order, value)
+	return bs.Put(bs.Order.PutUShort(value))
 }
 
-// PutSShort puts short(sign) from value to buffer
+// PutSShort puts a signed short with the order
 func (bs *OrderStream) PutSShort(value int16) error {
-	return Write(bs, bs.Order, value)
+	return bs.Put(bs.Order.PutShort(value))
 }
 
-// Int sets int got from buffer to value
+// Int get a signed int with the order
 func (bs *OrderStream) Int() (value int32, err error) {
-	return value, Read(bs, bs.Order, &value)
+	b, err := bs.get(IntSize)
+	if err != nil {
+		return 0, err
+	}
+
+	return bs.Order.Int(b), nil
 }
 
-// PutInt puts int from value to buffer
+// PutInt puts a signed int with the order
 func (bs *OrderStream) PutInt(value int32) error {
-	return Write(bs, bs.Order, value)
+	return bs.Put(bs.Order.PutInt(value))
 }
 
-// Long sets long got from buffer to value
+// UInt get an unsigned int with the order
+func (bs *OrderStream) UInt() (value uint32, err error) {
+	b, err := bs.get(IntSize)
+	if err != nil {
+		return 0, err
+	}
+
+	return bs.Order.UInt(b), nil
+}
+
+// PutUInt puts an unsigned int with the order
+func (bs *OrderStream) PutUInt(value uint32) error {
+	return bs.Put(bs.Order.PutUInt(value))
+}
+
+// Long gets a signed long with the order
 func (bs *OrderStream) Long() (value int64, err error) {
-	return value, Read(bs, bs.Order, &value)
+	b, err := bs.get(LongSize)
+	if err != nil {
+		return 0, err
+	}
+
+	return bs.Order.Long(b), nil
 }
 
-// PutLong puts long from value to buffer
+// PutLong puts a signed long with the order
 func (bs *OrderStream) PutLong(value int64) error {
-	return Write(bs, bs.Order, value)
+	return bs.Put(bs.Order.PutLong(value))
 }
 
-// Float sets float got from buffer to value
+// ULong gets an unsigned long with the order
+func (bs *OrderStream) ULong() (value uint64, err error) {
+	b, err := bs.get(LongSize)
+	if err != nil {
+		return 0, err
+	}
+
+	return bs.Order.ULong(b), nil
+}
+
+// PutULong puts an unsigned long with the order
+func (bs *OrderStream) PutULong(value uint64) error {
+	return bs.Put(bs.Order.PutULong(value))
+}
+
+// Float gets a float with the order
 func (bs *OrderStream) Float() (value float32, err error) {
-	return value, Read(bs, bs.Order, &value)
+	b, err := bs.get(FloatSize)
+	if err != nil {
+		return 0, err
+	}
+
+	return bs.Order.Float(b), nil
 }
 
-// PutFloat puts float from value to buffer
+// PutFloat puts a float with the order
 func (bs *OrderStream) PutFloat(value float32) error {
-	return Write(bs, bs.Order, value)
+	return bs.Put(bs.Order.PutFloat(value))
 }
 
-// Double sets double got from buffer to value
+// Double gets a double with the order
 func (bs *OrderStream) Double() (value float64, err error) {
-	return value, Read(bs, bs.Order, &value)
+	b, err := bs.get(DoubleSize)
+	if err != nil {
+		return 0, err
+	}
+
+	return bs.Order.Double(b), nil
 }
 
-// PutFloat puts double from value to buffer
+// PutDouble puts a double with the order
 func (bs *OrderStream) PutDouble(value float64) error {
-	return Write(bs, bs.Order, value)
+	return bs.Put(bs.Order.PutDouble(value))
 }
